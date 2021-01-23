d2fb731751cd486070f8fadf8e1251cca064340a
Sandeepa <nuwansandeepa3@gmail.com>
2021-01-23 10:35:14 +0530

    enable data binding

--- app/src/main/java/com/icbt/magula/ui/home/registration/RegistrationFragment.kt
+++ app/src/main/java/com/icbt/magula/ui/home/registration/RegistrationFragment.kt
@@ -9,24 +9,26 @@ import android.widget.TextView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.icbt.magula.R
+import com.icbt.magula.databinding.RegistrationFragmentBinding
 
 class RegistrationFragment : Fragment() {
 
     private val viewModel: RegistrationViewModel by viewModels()
+    private lateinit var binding:RegistrationFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view:View = inflater.inflate(R.layout.registration_fragment, container, false)
+        binding = RegistrationFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 
-        val t:TextView = view.findViewById(R.id.textView1)
-
-        viewModel.text.observe(viewLifecycleOwner, Observer {
-            t.text = it.toString()
-        })
-
-        return view
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            registrationViewModel = viewModel
+        }
     }
 
 }
