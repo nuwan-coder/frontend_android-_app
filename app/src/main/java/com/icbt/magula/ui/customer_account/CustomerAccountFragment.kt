fac9818497c3be4eb02fd9f62784f3217975546d
Sandeepa <nuwansandeepa3@gmail.com>
2021-01-25 12:08:58 +0530

    complete service_account_fragment.xml and customer_account_fragment.xml

--- app/src/main/java/com/icbt/magula/ui/customer_account/CustomerAccountFragment.kt
+++ app/src/main/java/com/icbt/magula/ui/customer_account/CustomerAccountFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.icbt.magula.R
 import com.icbt.magula.databinding.CustomerAccountFragmentBinding
 
@@ -21,6 +22,17 @@ class CustomerAccountFragment : Fragment() {
     ): View? {
         binding = CustomerAccountFragmentBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            customerAccountFragment = this@CustomerAccountFragment
+        }
+    }
+
+    fun onCancel(){
+        findNavController().navigate(R.id.action_customerAccountFragment_to_nav_home)
     }
 
 }
